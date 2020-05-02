4eeb9f50fb1adeade36970d5dc6372aa62d54d6c
wilbrone <wilbroneokoth@gmail.com>
2020-05-02 17:41:10 +0300

    adding order table and controller

--- app/Http/Controllers/orderController.php
+++ app/Http/Controllers/orderController.php
@@ -2,14 +2,40 @@
 
 namespace App\Http\Controllers;
 
-use App\Order;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+use App\Order;
+use App\Pizza;
 
 class orderController extends Controller
 {
-    public function store(Request $request){
-        $newOrder = Order::create($request->all());
 
-        return response()->json($newOrder, 200);
+    public function index(){
+        $data = app('db')->select("
+                SELECT * FROM orders
+                JOIN pizzas on orders.pizzaId = pizzas.id");
+
+        return response()->json($data);
+    }
+
+    public function store(Request $request){
+        // $newOrder = Order::create($request->all());
+        $newOrder = new Order;
+        $newOrder->pizzaId = $request->input('pizzaId');
+        $newOrder->orderNumber = Str::random();
+        $newOrder->quantity = $request->input('quantity');
+
+        // for calculating the total cost
+        $data = Pizza::where('id', '=', $newOrder->pizzaId)->first();
+
+        $qnt = $data->cost;
+        $untP = $newOrder->quantity;
+
+        $newOrder->totalCost = $qnt*$untP;
+
+
+        $newOrder->save();
+
+        return response()->json("Success!", 200);
     }
 }
--- app/Order.php
+++ app/Order.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model
 {
+    protected $primaryKey = 'orderId';
     protected $guarded = [];
+
+    public function pizzaId() {
+        return $this->hasOne('App\Pizza', 'pizzaId');
+    }
+
+    public function getRouteKeyName()
+    {
+        return $this->primaryKey;
+    }
 }
